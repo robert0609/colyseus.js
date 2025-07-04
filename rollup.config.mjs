95074157c6773e7cdcf9cefec9e36af690e74aa9
robert <robert0609@163.com>
2025-07-04 16:31:46 +0800

    feat: 修改构建配置，去除外部模块依赖

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -24,21 +24,47 @@ export default [
     // https://github.com/microsoft/TypeScript/issues/18442#issuecomment-749896695
     {
         input: ['src/index.ts'],
-        output: [{ preserveModules: true, banner, dir: 'build/esm', format: 'esm', entryFileNames: '[name].mjs', sourcemap: true },],
-        external,
+        output: [{ preserveModules: false, banner, dir: 'build/esm', format: 'esm', entryFileNames: '[name].mjs', sourcemap: true },],
         plugins: [
             replacePlugin,
             typescript({ tsconfig: './tsconfig/tsconfig.esm.json' }),
+            alias({
+                entries: [
+                    // httpie: force `fetch` for web environments
+                    { find: '@colyseus/httpie', replacement: path.resolve('./node_modules/@colyseus/httpie/fetch/index.js') },
+
+                    // ws: force browser.js version.
+                    { find: 'ws', replacement: path.resolve('./node_modules/ws/browser.js') },
+
+                    // @colyseus/schema: force browser version.
+                    { find: '@colyseus/schema', replacement: path.resolve('./node_modules/@colyseus/schema/build/umd/index.js') },
+                ]
+            }),
+            commonjs(),
+            nodeResolve({ browser: true }), // "browser" seems to have no effect here. (why??)
         ],
     },
 
     {
         input: ['src/index.ts'],
-        output: [{ preserveModules: true, banner, dir: 'build/cjs', format: 'cjs', entryFileNames: '[name].js', sourcemap: true },],
-        external,
+        output: [{ preserveModules: false, banner, dir: 'build/cjs', format: 'cjs', entryFileNames: '[name].js', sourcemap: true },],
         plugins: [
             replacePlugin,
             typescript({ tsconfig: './tsconfig/tsconfig.cjs.json' }),
+            alias({
+                entries: [
+                    // httpie: force `fetch` for web environments
+                    { find: '@colyseus/httpie', replacement: path.resolve('./node_modules/@colyseus/httpie/fetch/index.js') },
+
+                    // ws: force browser.js version.
+                    { find: 'ws', replacement: path.resolve('./node_modules/ws/browser.js') },
+
+                    // @colyseus/schema: force browser version.
+                    { find: '@colyseus/schema', replacement: path.resolve('./node_modules/@colyseus/schema/build/umd/index.js') },
+                ]
+            }),
+            commonjs(),
+            nodeResolve({ browser: true }), // "browser" seems to have no effect here. (why??)
         ],
     },
 
